14a36a00a116b9ba2745eca939d8cb8ab8e1b1d8
Jonatan <jonatan.stodeman@gmail.com>
2017-10-12 14:53:22 +0200

    add parsing for countdown timers (5m3h1s1w)

--- terminaltimer/daemon.py
+++ terminaltimer/daemon.py
@@ -1,16 +1,17 @@
 import datetime
+import re
 import uuid
 import zmq
 
 
 def dispatch(msg, timers):
-    if msg == 'add':
-        finish = datetime.datetime.now() + datetime.timedelta(hours=1)
-        add_timer('dummy timer', finish, timers)
-        return 'added'
+    # TODO: possibly register with decorator instead
+    # don't know what requirements are, probably best to just wait until more
+    # functionally complete or just risk having to undo it
+    if msg[:3] == 'add':
+        return add_timer(msg, timers)
     elif msg[:3] == 'del':
-        remove_timer(msg[4:], timers)
-        return 'removed'
+        return remove_timer(msg, timers)
     elif msg == 'list':
         return list_timers(timers)
     else:
@@ -19,9 +20,9 @@ def dispatch(msg, timers):
 
 def main(URL):
     # acquire socket or crash if already in use
-    ctx = zmq.Context.instance()
-    s = ctx.socket(zmq.REP)
-    s.bind(URL)
+    zmq_context = zmq.Context.instance()
+    socket = zmq_context.socket(zmq.REP)
+    socket.bind(URL)
 
     timers = {}
 
@@ -29,15 +30,22 @@ def main(URL):
     #   - Check incoming messages on socket
     #   - Check running alarms
     while True:
-        if s.poll(timeout=500):
-            incoming = s.recv().decode()
+        if socket.poll(timeout=500):
+            incoming = socket.recv().decode()
             outgoing = dispatch(incoming, timers)
-            s.send(outgoing.encode())
+            socket.send(outgoing.encode())
         check_timers()
 
 
-def remove_timer(identity, timers):
-    del timers[identity]
+def remove_timer(msg, timers):
+    args = msg.split(' ')
+    if len(args) != 2:
+        return f'bad command (this is a bug):\n{msg}'
+    command, identity = args
+    if identity in timers:
+        del timers[identity]
+        return 'removed'
+    return 'that timer doesn\'t exist.'
 
 
 def list_timers(timers):
@@ -47,9 +55,39 @@ def list_timers(timers):
     return str(result)
 
 
-def add_timer(description, finish_time, timers):
+def parse_time_description(finish):
+    time_units = {
+        'w': datetime.timedelta(weeks=1),
+        'd': datetime.timedelta(days=1),
+        'h': datetime.timedelta(hours=1),
+        'm': datetime.timedelta(minutes=1),
+        's': datetime.timedelta(seconds=1),
+    }
+    if re.match(r'(?i)^(\d+(?:y|d|w|h|m|s))+$', finish):
+        result = datetime.datetime.now()
+        split_finish = re.findall(r'(?i)(\d+)(y|d|w|h|m|s)', finish)
+        for amount, unit in split_finish:
+            amount = int(amount)
+            print(f'{amount!r}   {time_units[unit]!r}')
+            result += amount * time_units[unit]
+        return result
+    # TODO: add absolute time i.e. 12:37
+    return None
+
+
+def add_timer(msg, timers):
+    # todo parse duration/finish
+    args = msg.split(' ')
+    if len(args) < 2:
+        return f'expected at least two arguments'
+    command, finish, *description = args
+    description = ' '.join(description)
+    finish_time = parse_time_description(finish)
+    if finish_time is None:
+        return 'bad time format'
     timer = Timer(description, finish_time)
     timers[timer.identity] = timer
+    return 'added'
 
 
 def identity_dispenser():
