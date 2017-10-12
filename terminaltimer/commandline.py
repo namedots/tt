96cf4de82c1e6e14af7bf7bf599cc2b20004a461
Jonatan <jonatan.stodeman@gmail.com>
2017-10-12 17:14:44 +0200

    add one-off commands as args

--- terminaltimer/commandline.py
+++ terminaltimer/commandline.py
@@ -5,6 +5,7 @@ This is a dumb front-end to provide access to the daemon.
 
 import daemon
 import os
+import sys
 import zmq
 
 # importing directly modifies input() to behave like a shell's input with
@@ -17,28 +18,44 @@ URL = 'ipc://@TerminalTimer'
 def main():
     spawn_daemon()
 
+    # connect to daemon
     ctx = zmq.Context.instance()
     s = ctx.socket(zmq.REQ)
     s.connect(URL)
 
-    msg = ''
-    s.send(b'hello')  # it's only polite.
+    # one-off commands through arguments
+    if len(sys.argv) > 1:
+        args = sys.argv[1:]
+        s.send(' '.join(args).encode())
+        take_response(s)
+        sys.exit()
 
+    # poke the daemon to see if it has something to say or do before the user
+    # types anything in the interactive loop
+    s.send(b'hello')
+    take_response(s)
+
+    # interactive loop
     while True:
-        response = s.recv()
-        response = response.decode()
-        if response != '':
-            print(response)
-        if response == 'bye.':
-            break
-
         try:
             msg = input("> ").encode()
         except EOFError:
             break
         s.send(msg)
+        take_response(s)
+
+
+def take_response(s):
+    response = s.recv()
+    response = response.decode()
+    if response != '':
+        print(response)
+        if response == 'bye.':
+            sys.exit()
+
 
 def spawn_daemon():
+    # fork and daemonise the child which will run the daemon code
     if os.fork() == 0:
         with daemon.DaemonContext():
             from terminaltimer.daemon import main as daemon_main
